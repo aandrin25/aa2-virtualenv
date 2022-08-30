78f6b63af5a39f946aefbeef1c2b1b85791862a9
Averya <aandrin25@gmail.com>
2022-08-30 13:22:42 -0400

    Created a new Folder moved the first project into that folder.

---
+++ sxxharassmentapp.py
@@ -0,0 +1,9 @@
+#######################################################
+#Import Libraries
+#######################################################
+
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+#
